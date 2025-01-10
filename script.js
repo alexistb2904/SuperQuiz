4c3119e20ded60ac14b94c15aea1eec3401b658a
alexistb2904 <alexistb2904@gmail.com>
2025-01-10 15:39:14 +0100

    Last I hope

--- script.js
+++ script.js
@@ -1,20 +1,86 @@
 // Sélection de l'élément principal de l'application
 const app = document.getElementById('app');
+// Récupération des éléments audio
+const audio = new Audio('./sound/quizMusic.mp3');
+const correctAnswer = new Audio('./sound/correctAnswer.mp3');
+const wrongAnswer = new Audio('./sound/wrongAnswer.mp3');
 
-// Fonction pour gérer l'animation et la transition de la page d'accueil
-function startPageAnimation() {
-	// Recherche ou création de la page de démarrage
-	let startPage = document.querySelector('.startPage');
-	if (!startPage) {
-		startPage = document.createElement('div'); // Création d'un nouvel élément div
-		startPage.classList.add('startPage'); // Ajout d'une classe CSS
-		startPage.innerHTML = `
+let isMute = false;
+
+function createStartPage() {
+	stopMusic();
+	app.innerHTML = ''; // Réinitialisation du contenu de l'application
+	const startPage = document.createElement('div'); // Création d'un nouvel élément div
+	startPage.classList.add('startPage'); // Ajout d'une classe CSS
+	startPage.innerHTML = `
             <h1 class="firstText">Bienvenue</h1>
 			<h2 class="firstText">Sur</h2>
 			<h3 class="firstText">The Quiz</h3>
 			<button class="actionButton" onclick="startPageAnimation()">Jouer</button>
         `;
-		app.appendChild(startPage); // Ajout de la page au DOM
+	app.appendChild(startPage); // Ajout de la page au DOM
+}
+
+function playMusic() {
+	audio.play();
+	audio.volume = 0.15;
+	audio.loop = true;
+	console.log('Musique activée');
+	if (!document.querySelector('.volume')) {
+		const volume = document.createElement('div');
+		volume.classList.add('volume');
+		volume.innerHTML = `
+		<img src="./img/volume_on.svg" alt="volume">
+		`;
+
+		volume.querySelector('img').addEventListener('click', () => {
+			if (isMute) {
+				playMusic();
+				isMute = false;
+				volume.querySelector('img').src = './img/volume_on.svg';
+			} else {
+				pauseMusic();
+				isMute = true;
+				volume.querySelector('img').src = './img/volume_off.svg';
+			}
+		});
+
+		console.log(volume);
+		document.querySelector('.gamePage').appendChild(volume);
+	}
+}
+
+function pauseMusic() {
+	console.log('Musique mise sur pause');
+	audio.pause();
+}
+
+function stopMusic() {
+	audio.pause();
+	audio.currentTime = 0;
+
+	const volume = document.querySelector('.volume');
+	if (volume) {
+		volume.remove();
+	}
+}
+
+function correctAnswerSound() {
+	correctAnswer.play();
+}
+
+function wrongAnswerSound() {
+	wrongAnswer.play();
+}
+
+// Fonction pour gérer l'animation et la transition de la page d'accueil
+function startPageAnimation() {
+	stopMusic();
+	// Recherche ou création de la page de démarrage
+	let startPage = document.querySelector('.startPage');
+	if (!startPage) {
+		createStartPage();
+		startPage = document.querySelector('.startPage');
 	}
 
 	// Ajout d'une classe pour l'animation de sortie
@@ -30,6 +96,7 @@ function startPageAnimation() {
 // Fonction pour afficher la page de configuration
 function configPage() {
 	app.innerHTML = ''; // Réinitialisation du contenu de l'application
+	stopMusic();
 	const configPage = document.createElement('div'); // Création d'un nouvel élément div
 	configPage.classList.add('configPage'); // Ajout d'une classe CSS
 	configPage.innerHTML = `
@@ -63,23 +130,23 @@ function configPage() {
             <div class="configItem">
                 <label>
                 Temps pour chaque question
-                <input type="number" id="time" value="30" min="10" max="60">
+                <input type="number" id="time" value="15" min="10" max="60">
                 </label>
             </div>
         </div>
         <button class="actionButton">Commencer</button>
+		<button class="actionButton" onclick="createStartPage()">Retourner au menu</button>
     `;
 
 	app.appendChild(configPage); // Ajout de la page de configuration au DOM
 
 	// Ajout d'un gestionnaire d'événement pour le bouton "Commencer"
-	configPage.querySelector('button').addEventListener('click', () => {
+	configPage.querySelector('button:first-of-type').addEventListener('click', () => {
 		// Récupération des valeurs des champs de configuration
 		const amountOfQuestions = document.getElementById('amountOfQuestions').value;
 		const difficulty = document.getElementById('difficulty').value;
 		const results = document.getElementById('results').value;
 		const time = document.getElementById('time').value;
-
 		// Appel de la fonction pour démarrer le jeu
 		startGame(amountOfQuestions, difficulty, results, time);
 	});
@@ -93,9 +160,30 @@ async function startGame(amountOfQuestions, difficulty, results, time) {
 	console.log('Temps:', time);
 	console.log('Début du jeu');
 
-	app.innerHTML = ''; // Réinitialisation du contenu de l'application
+	document.querySelector('.configPage').remove(); // Suppression de la page de configuration
 	const gamePage = document.createElement('div'); // Création d'un nouvel élément div pour la page du jeu
 	gamePage.classList.add('gamePage'); // Ajout d'une classe CSS
+
+	let timerGlobal = parseInt((time * amountOfQuestions) / 1.5);
+	console.log('Temps global:', timerGlobal);
+	const timerGlobalDiv = document.createElement('span');
+	timerGlobalDiv.classList.add('timerGlobal');
+	timerGlobalDiv.innerHTML = `${timerGlobal}s`;
+
+	gamePage.appendChild(timerGlobalDiv);
+
+	const intervalGlobal = setInterval(() => {
+		timerGlobal--;
+		timerGlobalDiv.innerHTML = `${timerGlobal}s`;
+		if (timerGlobal === 0) {
+			clearInterval(intervalGlobal);
+			const nombreDeQuestionACompleter = amountOfQuestions - reponseQuestions.length;
+			for (let i = 0; i < nombreDeQuestionACompleter; i++) {
+				reponseQuestions.push('Pas de réponse');
+			}
+			showResult(questionsMelange, reponseQuestions);
+		}
+	}, 1000);
 
 	let questionsMelange = []; // Liste pour stocker les questions mélangées
 	console.log('Récupération des questions...');
@@ -126,6 +214,8 @@ async function startGame(amountOfQuestions, difficulty, results, time) {
 		});
 
 	let reponseQuestions = []; // Liste pour stocker les réponses de l'utilisateur
+
+	playMusic(); // Démarrage de la musique
 
 	// Fonction pour créer et afficher une question
 	async function createQuestion(question, index) {
@@ -167,36 +257,72 @@ async function startGame(amountOfQuestions, difficulty, results, time) {
                 <button class="actionButton">${index === questionsMelange.length ? 'Résultats' : 'Suivant'}</button>
             `;
 
+			questionContainer.querySelector('.reponses').animate(
+				[
+					{ transform: 'translateX(-100%)', opacity: 0 },
+					{ transform: 'translateX(0)', opacity: 1 },
+				],
+				{
+					duration: 1000,
+					easing: 'ease-in-out',
+				}
+			);
+
+			questionContainer.querySelector('.question').animate(
+				[
+					{ transform: 'translateY(-100%)', opacity: 0 },
+					{ transform: 'translateY(0)', opacity: 1 },
+				],
+				{
+					duration: 1000,
+					easing: 'ease-in-out',
+				}
+			);
+
 			// Gestionnaire d'événement pour le bouton "Suivant" ou "Résultats"
 			questionContainer.querySelector('.actionButton').addEventListener('click', () => {
 				// Récupération de la réponse sélectionnée par l'utilisateur
-				const reponses = document.querySelectorAll('.radio');
+				const reponses = questionContainer.querySelectorAll(`.radio`);
 				let reponseToPush = '';
 				reponses.forEach((reponse) => {
 					if (reponse.querySelector('input').checked) {
 						reponseToPush = reponse.querySelector('.reponse').textContent;
 					}
 				});
+				console.log('Réponse sélectionnée:', reponseToPush);
+				console.log('Réponse correcte:', question.answer);
 
 				// Si une réponse est sélectionnée ou si le temps est écoulé
 				if (reponseToPush || tempsRestant === 0) {
-					reponseQuestions.push(reponseToPush); // Ajout de la réponse à la liste
+					reponseQuestions.push(reponseToPush == '' ? 'Pas de réponse' : reponseToPush); // Ajout de la réponse à la liste
 					if (results == 'instant') {
+						clearInterval(interval);
+						if (reponseToPush === question.answer) {
+							correctAnswerSound();
+						} else {
+							wrongAnswerSound();
+						}
+
 						questionContainer.innerHTML = `
                         <h1 class="question">${question.question}</h1>
                         <span class="counter">${index}/${questionsMelange.length}</span>
-                        <div class="reponses">
+                        <div class="reponses background_${reponseToPush === question.answer}">
                             <div class="reponse">Votre réponse: ${reponseToPush}</div>
                             <div class="reponse">Réponse correcte: ${question.answer}</div>
                         </div>
-                        <div>Passage à la question suivante dans 3 secondes...</div>`;
+                        <div>Passage à la question suivante dans 2 secondes...</div>`;
 						setTimeout(() => {
 							questionContainer.remove(); // Suppression de la question affichée
 							resolve(); // Passage à la question suivante
-						}, 3000);
+						}, 2000);
 					} else {
-						questionContainer.remove(); // Suppression de la question affichée
-						resolve(); // Passage à la question
+						clearInterval(interval);
+						questionContainer.querySelector('.reponses').classList.add('exitAnimation');
+						questionContainer.querySelector('.question').classList.add('exitAnimation');
+						setTimeout(() => {
+							questionContainer.remove(); // Suppression de la question affichée
+							resolve(); // Passage à la question suivante
+						}, 1000);
 					}
 				} else {
 					alert('Vous devez choisir une réponse'); // Alerte si aucune réponse n'est sélectionnée
@@ -208,11 +334,18 @@ async function startGame(amountOfQuestions, difficulty, results, time) {
 			// Gestion du chronomètre
 			const timer = document.querySelector('.timer');
 			const interval = setInterval(() => {
-				tempsRestant--; // Décrémentation du temps restant
-				timer.innerHTML = tempsRestant + 's'; // Mise à jour de l'affichage du temps
-				if (tempsRestant === 0) {
-					clearInterval(interval); // Arrêt du chronomètre
-					questionContainer.querySelector('.actionButton').click(); // Passage automatique à la question suivante
+				if (timerGlobal > 0) {
+					tempsRestant--; // Décrémentation du temps restant
+					timer.innerHTML = tempsRestant + 's'; // Mise à jour de l'affichage du temps
+					if (tempsRestant === 0) {
+						clearInterval(interval); // Arrêt du chronomètre
+						console.log('Temps écoulé');
+						if (questionContainer.querySelector('.actionButton')) {
+							questionContainer.querySelector('.actionButton').click(); // Passage automatique à la question suivante
+						}
+					}
+				} else {
+					clearInterval(interval);
 				}
 			}, 1000);
 		});
@@ -220,11 +353,15 @@ async function startGame(amountOfQuestions, difficulty, results, time) {
 
 	// Création et affichage des questions
 	for (const [index, question] of questionsMelange.entries()) {
-		await createQuestion(question, index + 1);
+		if (timerGlobal > 0) {
+			await createQuestion(question, index + 1);
+		}
 	}
 
 	console.log('Fin du jeu');
-	showResult(questionsMelange, reponseQuestions); // Affichage des résultats
+	if (timerGlobal > 0) {
+		showResult(questionsMelange, reponseQuestions); // Affichage des résultats
+	}
 
 	// Fonction pour afficher les résultats
 	function showResult(questions, reponses) {
